18b00d7798488bdd158b3a287c3eeb6ff89032ba
mechack2022 <dtoms624@gmail.com>
2023-08-08 03:07:26 +0100

    creation of kafka producer-app

---
+++ src/main/java/com/fragile/kafkainstallation/KafkaInstallationApplication.java
@@ -0,0 +1,13 @@
+package com.fragile.kafkainstallation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaInstallationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaInstallationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fragile/kafkainstallation/KafkaInstallationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fragile.kafkainstallation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaInstallationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
